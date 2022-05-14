b44a4add7f09c76b5e3e31a6bbbbd2c7e0fc7a1a
XavusZookie <xavuszookie@gmail.com>
2022-05-14 02:32:58 -0400

    gamemanager

---
+++ Project 3vi1_/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager 
+{
+    public static int boss_health;
+    public static int boss_shape;
+
+    public static int boss_attack_type;
+    public static int boss_attack_range;
+    public static int boss_attack_speed;
+    public static int boss_attack_damage;
+    public static int boss_attack_directions;
+
+    public static int boss_move_speed;
+    public static int boss_move_pattern;
+
+    public static int spawns_on;
+    public static int spawns_health;
+
+
+    public static int spawns_move_speed;
+    public static int spawns_attack_damage;
+    public static int spawns_attack_speed;
+    public static int spawns_attack_range;
+
+
+
+}
