ace8cd2c83f5636ba9a44e898ac15199613201f9
XavusZookie <xavuszookie@gmail.com>
2022-05-23 19:02:11 -0400

    opening sequence
    title screen and door opening with fade and fast track

---
+++ Project 3vi1_/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public Transform leftdoor;
+    public Transform rightdoor;
+    public float speed = 3f;
+    public bool clicked = false;
+    public CanvasGroup fader;
+    public GameObject butt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        if (clicked == true)
+        {
+            leftdoor.Translate(Vector3.up * Time.deltaTime * speed);
+
+            rightdoor.Translate(Vector3.down * Time.deltaTime * speed);
+
+            fader.alpha = (fader.alpha -.3f * Time.deltaTime);
+
+        }
+
+        if (rightdoor.position.x >= 2500f )
+        {
+            clicked = false;
+        }
+
+    }
+
+    public void enterbutton()
+    {
+        clicked = true;
+    }
+
+    public void clickaway()
+    {
+        leftdoor.position = new Vector3(-2500f,0,0);
+        rightdoor.position = new Vector3(2500f, 0, 0);
+        fader.alpha = 0;
+        Destroy(butt);
+    }
+
+}
